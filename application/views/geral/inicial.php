52121500d0e2f99d39c6f59e10af13b2ae774e50
Celso Junior <clsjunnior@live.com>
2016-08-29 01:34:15 -0300

    maps

--- application/views/geral/inicial.php
+++ application/views/geral/inicial.php
@@ -22,7 +22,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link rel="stylesheet" href="<?=base_url('assets/fonts/font-awesome.min.css')?>">
 	<!-- Ionicons -->
 	<link rel="stylesheet" href="<?=base_url('assets/fonts//ionicons.min.css')?>">
-
+	<!-- Estilo -->
+	<link rel="stylesheet" href="<?=base_url('assets/css/estilo_geral.css')?>">
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -31,13 +32,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 	<div class="row">
+		<div class="banner-inicio">
+			<img src="<?=base_url('assets/third_party/logo/logo-branco.png')?>">
+		</div>
+		<div class="sub-menu">
 
-
+		</div>
 	</div>
+	<div class="row">
+		<div class="col-lg-3 col-md-4 col-xs-12 menu-lateral">
 
-<!-- jQuery 2.2.3 -->
-<script src="<?=base_url('assets/plugins/jQuery/jquery-2.2.3.min.js')?>"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="<?=base_url('assets/js/bootstrap.js')?>"></script>
+		</div>
+		<div class="col-lg-9 col-md-8 col-xs-12 mapa" >
+			<div id="map"></div>
+
+		</div>
+	</div>
+	<!-- jQuery 2.2.3 -->
+	<script src="<?=base_url('assets/plugins/jQuery/jquery-2.2.3.min.js')?>"></script>
+	<!-- MAPS -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_cPiH8zBAbz360dlO4I1wrrqVgKvN4z4&callback=initMap"
+			async defer></script>
+	<!-- Bootstrap 3.3.6 -->
+	<script src="<?=base_url('assets/js/bootstrap.js')?>"></script>
+
+	<script>
+		function initMap() {
+			// Create a map object and specify the DOM element for display.
+			var map = new google.maps.Map(document.getElementById('map'), {
+				center: {lat: -21.673391, lng: -49.747130},
+				scrollwheel: false,
+				zoom: 8
+			});
+		}
+	</script>
 </body>
 </html>
