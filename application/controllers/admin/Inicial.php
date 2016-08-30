589bcec9dcfede49e71d8c60273cd01fc04487d5
leobelini <leobelini96@gmail.com>
2016-08-29 22:28:45 -0300

    Parametro com os dados do usuario passado para a view.

--- application/controllers/admin/Inicial.php
+++ application/controllers/admin/Inicial.php
@@ -4,6 +4,8 @@ class Inicial extends CI_Controller {
 
 	public function index(){
 	    $dados['title'] = "Inicial";
+        $dados['user'] = $this->session->userdata('user');
+
 	    $this->load->view('admin/inicio',$dados);
 	}
 
