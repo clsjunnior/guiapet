1dd36e2170ac94614289892ab59c7bd60e6e1359
leobelini <leobelini96@gmail.com>
2016-08-29 22:20:16 -0300

    Configuração do header

--- application/controllers/admin/Inicial.php
+++ application/controllers/admin/Inicial.php
@@ -3,7 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Inicial extends CI_Controller {
 
 	public function index(){
-	    $this->load->view('admin/inicio');
+	    $dados['title'] = "Inicial";
+	    $this->load->view('admin/inicio',$dados);
 	}
 
 }
